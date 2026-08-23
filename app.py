97dfa405c474036727b35881268493627cdf9645
Aaron-Limbu <aaronlimbu381@gmail.com>
2026-08-23 16:26:37 +0545

    if hell

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from colorama import Fore, Back, Style, init
 
 #weblog = '127.0.0.1 - - [20/Aug/2026:12:30:00 +0000] "GET /index.html HTTP/1.1" 200 2326'
 
-weblog = '127.0.0.1 - - [20/Aug/2026:12:30:00 +0000] "GET /.env HTTP/1.1" 200 2326'
-table = {}
+#weblog = '127.0.0.1 - - [20/Aug/2026:12:30:00 +0000] "GET /.env HTTP/1.1" 200 2326'
+table = {
+    "ip":[],
+    "date":[],
+    "content":[],
+    "status":[],
+}
 """
     For analyzing weblog i create list of files or path to files for checking what is being accessed 
     rather than this writing every path to sensitive file for find it on log i will just use extensions like .conf .log 
@@ -40,6 +45,20 @@ status_codes = [
     '200',"404","304","403","404","500"
 ]
 
+badf_ext = [
+    '.env',
+    '.log',
+    '.txt',
+    '.ini',
+]
+
+goodf_ext = [
+    '.html',
+    '.php',
+    '.png',
+    '.css',
+]
+
 class LogAnalyzer: 
     def __init__(self,weblog,table):
         self.weblog = weblog
@@ -50,14 +69,15 @@ class LogAnalyzer:
         found_ip = re.findall(ip_pattern,self.weblog)
         for ip in found_ip:
             print(f"[+] ip found : {ip}")
-            self.table["ip"] = ip
+            self.table["ip"].append(ip)
+
         date_pattern = r"\[\d{1,2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}\]"        
         found_date = []
         matched = re.search(date_pattern,self.weblog)
         found_date.append(matched.group())
         for date in found_date: 
             print(f"[+] date: {date}")
-            self.table["date"] = date
+            self.table["date"].append(date)
         self.analyzeReq()
 
         
@@ -68,23 +88,33 @@ class LogAnalyzer:
         get_contents = re.findall(r'"([^"]*)"',self.weblog)
         found_method = re.search(r'"[A-Za-z]{2}',self.weblog) 
         method = ""
-        self.table["content"] = get_contents
+        self.table["content"].append(get_contents)
+        print(f"[+] Content : {get_contents}")
         for m in methods :
             if found_method.group()[1:3] == m[:2]:
                 method = m
-        
+
         if method == "GET":
-            f_match = [n for n in not_good if not_good if n in get_contents[0]] 
+            f_match = [n for n in badf_ext if badf_ext if n in get_contents[0]] 
             if f_match:
                 print(Fore.YELLOW + f"[i] Suspicious request for FILE : {f_match[0]}")
+            else: 
+                f_match = [g for g in goodf_ext if goodf_ext if g in get_contents[0]]
+                if f_match:
+                    print(Fore.GREEN + f"[+] Normal request : {f_match[0]}")
+            
             reqS = [s for s in status_codes if status_codes if s in self.weblog] #might have overcomplicated this#-_- idk
-            match reqS: 
+            """match reqS: 
                 case ['200']:
                     print(Fore.RED + f"[!] Status code: {reqS[0]} for {f_match[0]} from IP: {self.table['ip']}")
+                    self.table["status"].append(reqS)
                 case ['403']:
                     print(Fore.YELLOW + f"[i] Status code: {reqS[0]} for {f_match[0]} (still suspicious) from IP: {self.table['ip']}")
+                    self.table["status"].append(reqS)
                 case _: 
                     print("[-] status not found")
+                    self.table["status"].append("not found")
+"""
     def returntable(self):
         print(f"[+] {self.table}")
         return self.table
@@ -92,8 +122,14 @@ class LogAnalyzer:
 if __name__ == "__main__":
     try: 
         init(autoreset=True)
-        loganalyzer = LogAnalyzer(weblog,table)
-        loganalyzer.analyzeLog()
-        loganalyzer.returntable()
+        print("[i] Checking Web Logs") 
+        for i,wlog in enumerate(norm) : 
+            print(f"[+] id {i} : \'{wlog}\'")
+            loganalyzer = LogAnalyzer(wlog,table)
+            loganalyzer.analyzeLog()
+            loganalyzer.returntable()
+        #loganalyzer = LogAnalyzer(weblog,table)
+        #loganalyzer.analyzeLog()
+        #loganalyzer.returntable()
     except Exception as e:
         print(f"[!] {e}")
