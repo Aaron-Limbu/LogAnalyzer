05f24857a98684fcb3a1f2bb8b012bc6dec0d715
Aaron-Limbu <aaronlimbu381@gmail.com>
2026-09-02 01:25:41 +0545

    if hell 2.2

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from colorama import Fore, Back, Style, init
 #weblog = '127.0.0.1 - - [20/Aug/2026:12:30:00 +0000] "GET /index.html HTTP/1.1" 200 2326'
 
 #weblog = '127.0.0.1 - - [20/Aug/2026:12:30:00 +0000] "GET /.env HTTP/1.1" 200 2326'
-table = {
+entry = {
     "ip":[],
     "date":[],
     "content":[],
     "status":[],
 }
+
+LogTable = []
 """
     For analyzing weblog i create list of files or path to files for checking what is being accessed 
     rather than this writing every path to sensitive file for find it on log i will just use extensions like .conf .log 
@@ -31,9 +33,11 @@ mr = ['185.220.101.5 - - [22/Aug/2026:01:22:14 +0000] "GET /wp-admin/ HTTP/1.1" 
 '141.98.11.44 - - [22/Aug/2026:04:05:19 +0000] "GET /shell?cd+/tmp;rm+-rf+*;wget+... HTTP/1.1" 400 312']
 
 # client & server errors
-cserror = ['192.168.1.100 - - [22/Aug/2026:14:20:05 +0000] "GET /bad-link HTTP/1.1" 404 1245',
-'172.217.16.142 - - [22/Aug/2026:15:32:41 +0000] "POST /checkout HTTP/1.1" 500 562',
-'10.0.0.5 - - [22/Aug/2026:16:01:12 +0000] "GET /heavy-report HTTP/1.1" 504 0']
+cserror = [
+        '192.168.1.100 - - [22/Aug/2026:14:20:05 +0000] "GET /bad-link HTTP/1.1" 404 1245',
+        '172.217.16.142 - - [22/Aug/2026:15:32:41 +0000] "POST /checkout HTTP/1.1" 500 562',
+        '10.0.0.5 - - [22/Aug/2026:16:01:12 +0000] "GET /heavy-report HTTP/1.1" 504 0'
+]
 
 
 not_good = [
@@ -88,8 +92,13 @@ rce_cmds = [
     'ls',
 ]
 
+common_endpoints = [
+    '/checkout',
+    '/heavy-report',
+]
+
 api_endpoints = [
-    '/api/v1/login'
+    '/api/v1/login',
 ]
 
 
@@ -128,11 +137,18 @@ class LogAnalyzer:
         print(f"[+] Content : {get_contents}")
         parts = request_line.split()
         method = parts[0].upper() if parts else ""
+        logsplit = self.weblog.split()
+        status = logsplit[8] 
+        self.table["status"].append(status)
+        
         if method not in methods:
             print(f"[-] Unknown method: {method}")
             return
         match method:
             case "GET":
+
+                if status in ["404","500","504"] and request_line.split()[1] not in common_endpoints:
+                    print(Fore.YELLOW + f"[-] Server Error endpoint : {request_line.split()[1]}")
                 f_match = [n for n in badf_ext if n in request_line]
                 if f_match:
                     print(Fore.YELLOW + f"[i] Suspicious request for FILE : {f_match[0]}")
@@ -145,6 +161,10 @@ class LogAnalyzer:
                     if ce_match:
                         print(Fore.RED + f"[!] Command Found : {request_line.split()[1] if len(request_line.split()) > 1 else request_line}")
 
+                    e_match = [e for e in common_endpoints if e in request_line]
+                    if e_match:
+                        if status in ["404","500","504"]:
+                            print(Fore.YELLOW + f"[-] Server Error endpoint : {e_match[0]}")
                     f_match = [g for g in goodf_ext if g in request_line]
                     if f_match:
 
@@ -169,16 +189,24 @@ class LogAnalyzer:
                 pf_match = [f for f in protectedFiles if f in request_line]
                 if pf_match:
                     print(Fore.RED + f"[!] POST REQUEST ON PROTECTED FILE: {pf_match[0]}")
+                e_match = [e for e in common_endpoints if e in request_line]
+                if e_match:
+                    if status in ["404","500","504"]:
+                        print(Fore.YELLOW + f"[-] Server Error endpoint : {e_match[0]}")
 
             case _:
                 print(f"[-] Unhandled method: {method}")
+
+    def ranking(self,rank):
+        logRank = []
+        logRank.append(self.weblog) 
+
 
     def returntable(self):
         print("-------------------------------")
         print("[+] Table")
         for i in range(len(self.table["ip"])): 
-            print(f"[{i}] IP : {self.table['ip'][i]}, DATE : {self.table['date'][i]}, CONTENT : {self.table['content'][i]}")
-            #print(f"[{i}] STATUS : {self.table['status'][i]}")
+            print(f"[{i}] IP : {self.table['ip'][i]}, DATE : {self.table['date'][i]}, CONTENT : {self.table['content'][i]}, [{i}] STATUS CODE: {self.table['status'][i]}")
 
 if __name__ == "__main__":
     try: 
@@ -186,11 +214,16 @@ if __name__ == "__main__":
         print("[i] Checking Web Logs") 
         for i,wlog in enumerate(norm) : 
             print(f"[+] id {i} : \'{wlog}\'")
-            loganalyzer = LogAnalyzer(wlog,table)
+            loganalyzer = LogAnalyzer(wlog,entry)
             loganalyzer.analyzeLog()
         for i,wlog in enumerate(mr): 
             print(f"[+] id {i} : \'{wlog}\'")
-            loganalyzer = LogAnalyzer(wlog,table)
+            loganalyzer = LogAnalyzer(wlog,entry)
+            loganalyzer.analyzeLog()
+        for i,wlog in enumerate(cserror) :
+            print(wlog)
+            print(f"[+] id {i} : \'{wlog}\'")
+            loganalyzer = LogAnalyzer(wlog,entry)
             loganalyzer.analyzeLog()
         #loganalyzer = LogAnalyzer(weblog,table)
         #loganalyzer.analyzeLog()
